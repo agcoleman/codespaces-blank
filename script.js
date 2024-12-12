4f7f1080c1d7e38a6f9e6a3825ea8ed4ece866bd
agcoleman <agcoleman@cps.edu>
2024-12-12 14:43:25 +0000

    gg

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let monster = document .querySelector('.monster');
 let bigmonk = document .querySelector('.bigmonk');
 let Kingkong = document .querySelector('.Kingkong');
 let monkey = document .querySelector('.monkey');
+let bum = document .querySelector('.bum');
 godzilla.style.display ="none";
 monster.style.display ="none";
 monkey.style.display= "none";
@@ -20,6 +21,7 @@ left.addEventListener( 'click' ,function() {
     left.style.display ="none";
     right.style.display ="none";
     godzilla.style.display ="block";
+    bum.innerHTML="Godzilla is ready to fight dbl click to fight king kong use scroll wheel to be peaceful"
 })
 
 godzilla.addEventListener( 'wheel' ,function() {
@@ -29,6 +31,7 @@ godzilla.addEventListener( 'wheel' ,function() {
     right.style.display ="none";
     godzilla.style.display ="none";
     monster.style.display ="block";
+    bum.innerHTML="Godzilla decided king kong didnt deserve a beating today"
 })
 
 //
@@ -41,4 +44,39 @@ godzilla.addEventListener( 'dblclick' ,function() {
     right.style.display ="none";
     godzilla.style.display ="none";
     monkey.style.display ="block";
+      bum.innerHTML="Godzilla is now ready to face king kong in a fight"
+})
+
+
+
+right.addEventListener( 'click' ,function() {
+
+    LeftRight.style.display ="none";
+    left.style.display ="none";
+    right.style.display ="none";
+    Kingkong.style.display ="block";
+    bum.innerHTML="King Kong is ready to fight dbl click to fight king kong use scroll wheel to be peaceful"
+})
+
+Kingkong.addEventListener( 'wheel' ,function() {
+
+    LeftRight.style.display ="none";
+    left.style.display ="none";
+    right.style.display ="none";
+    Kingkong.style.display ="none";
+    bigmonk.style.display ="block";
+    bum.innerHTML="Godzilla decided king kong didnt deserve a beating today"
+})
+
+//
+
+
+Kingkong.addEventListener( 'dblclick' ,function() {
+
+    LeftRight.style.display ="none";
+    left.style.display ="none";
+    right.style.display ="none";
+    Kingkong.style.display ="none";
+    monkey.style.display ="block";
+      bum.innerHTML="Godzilla is now ready to face king kong in a fight"
 })
